03414f1ed8e9783b43d6063fdc985b77bbc8cc05
DavidUllauri <30758544+DavidUllauri@users.noreply.github.com>
2017-08-06 10:29:06 -0400

    Class Vs Struct in c#

---
+++ Assets/Player/MyReplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public struct MyKeyFrame {
+
+    private float time;         // The time we are storing this event
+    private Vector3 position;
+    private Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 position, Quaternion rotation) {
+        this.time = time;
+        this.position = position;
+        this.rotation = rotation;
+    }
+}
